248ba1041760e15ecadb57544a4d66dee5e3b217
orstavik <orstavik77@gmail.com>
2017-03-21 15:04:55 +0100

    added persistence files, but not integrated them yet

---
+++ sw.js
@@ -0,0 +1,7 @@
+importScripts('/src/js/sw-lib.min.js');
+const cdnCacheStrategy = goog.swlib.cacheFirst({cacheableResponse: {statuses: [0]}});
+goog.swlib.router.registerRoute(new RegExp('^https://www.gstatic.com/firebasejs/3.7.2/firebase.js'), cdnCacheStrategy);
+goog.swlib.router.registerRoute('/sw.js', goog.swlib.networkFirst());
+goog.swlib.router.registerRoute('/', goog.swlib.staleWhileRevalidate());
+goog.swlib.router.registerRoute('/index.html', goog.swlib.staleWhileRevalidate());
+goog.swlib.router.registerRoute(new RegExp('.html|.css|.js'), goog.swlib.cacheFirst());
