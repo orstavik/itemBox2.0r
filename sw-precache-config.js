2ac5620c82f4b33d2a6fbf125412f394243dc074
enlightenmentor <fales.tom.1994@gmail.com>
2017-04-19 12:43:57 +0300

    configuration

---
+++ sw-precache-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  staticFileGlobs: [
+    '/index.html',
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-loader.js'
+  ],
+  cacheId: "itembox-cache",
+  navigateFallback: '/index.html'
+};
