30ea8c882ed8abff7c72ba2d7e96140f008d1b82
orstavik <orstavik77@gmail.com>
2017-04-27 10:49:42 +0200

    fixing bugs

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,22 @@ exports.makeSketchListNamePropView =
  functions.database.ref('/users/{userID}/sketches/{sketchID}/info').onWrite(event => {
    const original = event.data.val();
    let newPath = "/viewSketchList/users/" + event.params.userID + "/sketches/" + event.params.sketchID + "/info";
-   console.log("ivar is the king of functions");
+   console.log("ivar and tom are the king of functions");
    admin.database().ref(newPath).set(original);
-  //  return event.data.ref.root.ref(newPath).set(original);
  });
+
+//create on the client side a firebase-I-shall-say-this-only-once wc,
+//that connects to a path "/systemToClient/users/" + user.userID + "/systemMessage/", and every time a new entry is made,
+//it reads that entry out to the user, and then after it has downloaded it, it deletes it from the datastore.
+// exports.sendWelcomeMessage = functions.auth.user().onCreate(event => {
+//   const user = event.data;
+//   let newPath = "/systemToClient/users/" + user.userID + "/systemMessage/";
+//   console.log("How are the plans for moving to Lviv going? :)");
+//   admin.database().ref(newPath).push({msg: "Welcome to Robotpad and the drawing of petals!", timeStamp: new Date()});
+// });
+//      _newUser(e) { //todo this we want to do as a firebase cloud function
+//        this.$.serverUserSnap.updateOverwrite({
+//          registered: firebase.database.ServerValue.TIMESTAMP
+//        });
+//        this._fire("system-message", "Welcome new user!");
+//      }
