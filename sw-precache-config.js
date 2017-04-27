c9fa09a746e0082c3b5a82b128c9c23a8cc8611e
enlightenmentor <fales.tom.1994@gmail.com>
2017-04-27 17:40:14 +0300

    polymer build is great! we go offline!!

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -5,5 +5,30 @@ module.exports = {
     '/bower_components/webcomponentsjs/webcomponents-loader.js'
   ],
   cacheId: "itembox-cache",
-  navigateFallback: '/index.html'
+  navigateFallback: '/index.html',
+  runtimeCaching: [{
+    urlPattern: /^https:\/\/www.gstatic\.com\/firebasejs/,
+    handler: 'networkFirst',
+    options: {
+      cache: {
+        name: 'firebase-cache'
+      }
+    }
+  },{
+    urlPattern: /\/src/,
+    handler: 'fastest',
+    options: {
+      cache: {
+        name: 'src-cache'
+      }
+    }
+  },{
+    urlPattern: /\/img/,
+    handler: 'fastest',
+    options: {
+      cache: {
+        name: 'img-cache'
+      }
+    }
+  }]
 };
